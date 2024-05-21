7959d3967ca79fc92b62e7bbbf992b80681e1669
Vitor Pires <vitorpires255@hotmail.com>
2024-05-21 05:14:21 -0300

    Voxel type changed from enum to struct

--- Assets/Scripts/VoxelSys/ChunkPersistance.cs
+++ Assets/Scripts/VoxelSys/ChunkPersistance.cs
@@ -4,92 +4,93 @@ using System;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Collections.Generic;
+using System.Linq;
 
 public partial class Chunk : MonoBehaviour
 {
 
-    public bool modified = false;
+	public bool modified = false;
 
-    public void Save()
-    {
-        string path = SaveUtil.GetChunkPath(chunkPosition);
+	public void Save()
+	{
+		string path = SaveUtil.GetChunkPath(chunkPosition);
 
-        ChunkData data = new ChunkData(this);
+		ChunkData data = new ChunkData(this);
 
-        BinaryFormatter formatter = new BinaryFormatter();
-        FileStream stream = new FileStream(path, FileMode.Create);
-        formatter.Serialize(stream, data);
-        stream.Close();
-    }
+		BinaryFormatter formatter = new BinaryFormatter();
+		FileStream stream = new FileStream(path, FileMode.Create);
+		formatter.Serialize(stream, data);
+		stream.Close();
+	}
 
-    public bool TryInitByLoad()
-    {
-        // Tries to get save file
-        string path = SaveUtil.GetChunkPath(chunkPosition);
+	public bool TryInitByLoad()
+	{
+		// Tries to get save file
+		string path = SaveUtil.GetChunkPath(chunkPosition);
 
-        if (!File.Exists(path))
-        {
-            return false;
-        }
+		if (!File.Exists(path))
+		{
+			return false;
+		}
 
-        // If it succeeds, loads the file
+		// If it succeeds, loads the file
 
-        BinaryFormatter formatter = new BinaryFormatter();
-        FileStream stream = new FileStream(path, FileMode.Open);
-        ChunkData data = formatter.Deserialize(stream) as ChunkData;
-        this.voxels = (Voxel[])(object)data.voxelData;
-        foreach (WorldObjectData wid in data.worldItemData)
-        {
-            SpawnManager.Main.SpawnByLoad(wid, out WorldObject worldItem);
-            AddItem(worldItem);
-        }
-        stream.Close();
+		BinaryFormatter formatter = new BinaryFormatter();
+		FileStream stream = new FileStream(path, FileMode.Open);
+		ChunkData data = formatter.Deserialize(stream) as ChunkData;
+		this.voxels = data.voxelData.Select(c => (Voxel)c).ToArray();
+		foreach (WorldObjectData wid in data.worldItemData)
+		{
+			SpawnManager.Main.SpawnByLoad(wid, out WorldObject worldItem);
+			AddItem(worldItem);
+		}
+		stream.Close();
 
-        Dirty = true;
-        Initialized = true;
-        Itemized = true;
-        return true;
-    }
+		Dirty = true;
+		Initialized = true;
+		Itemized = true;
+		return true;
+	}
 
-    HashSet<WorldObject> worldItems = new HashSet<WorldObject>();
+	HashSet<WorldObject> worldItems = new HashSet<WorldObject>();
 
-    public void AddItem(WorldObject worldItem)
-    {
-        worldItems.Add(worldItem);
-        modified = true;
-    }
+	public void AddItem(WorldObject worldItem)
+	{
+		worldItems.Add(worldItem);
+		modified = true;
+	}
 
-    public void RemoveItem(WorldObject worldItem)
-    {
-        worldItems.Remove(worldItem);
-        modified = true;
-    }
+	public void RemoveItem(WorldObject worldItem)
+	{
+		worldItems.Remove(worldItem);
+		modified = true;
+	}
 
-    public void DestroyAllItems()
-    {
-        foreach (WorldObject wi in worldItems)
-        {
-            Destroy(wi.gameObject);
-        }
-        worldItems.Clear();
-    }
+	public void DestroyAllItems()
+	{
+		foreach (WorldObject wi in worldItems)
+		{
+			Destroy(wi.gameObject);
+		}
+		worldItems.Clear();
+	}
 
-    [Serializable]
-    public class ChunkData // Maybe do nested data classes for all serializables?
-    {
-        public byte[] voxelData;
+	[Serializable]
+	public class ChunkData // Maybe do nested data classes for all serializables?
+	{
+		public byte[] voxelData;
 
-        public WorldObjectData[] worldItemData;
+		public WorldObjectData[] worldItemData;
 
-        public ChunkData(Chunk chunk)
-        {
-            voxelData = Array.ConvertAll(chunk.voxels, new Converter<Voxel, byte>(SaveUtil.VoxelToByte));
-            worldItemData = new WorldObjectData[chunk.worldItems.Count];
-            int i = 0;
-            foreach (WorldObject wi in chunk.worldItems)
-            {
-                worldItemData[i++] = wi.GetData();
-            }
-        }
-    }
+		public ChunkData(Chunk chunk)
+		{
+			voxelData = Array.ConvertAll(chunk.voxels, new Converter<Voxel, byte>(SaveUtil.VoxelToByte));
+			worldItemData = new WorldObjectData[chunk.worldItems.Count];
+			int i = 0;
+			foreach (WorldObject wi in chunk.worldItems)
+			{
+				worldItemData[i++] = wi.GetData();
+			}
+		}
+	}
 }
--- Assets/Scripts/VoxelSys/Voxel.cs
+++ Assets/Scripts/VoxelSys/Voxel.cs
@@ -1,3 +1,23 @@
 ﻿
 // RULE: Air comes first, then block types, then non-block types
-public enum Voxel : byte { Air, Stone, Brick, Magic, Dirt, Grass, Object }
+public struct Voxel
+{
+	private byte m_Type;
+
+	public static Voxel Air => new(0);
+	public static Voxel Stone => new(1);
+	public static Voxel Brick => new(2);
+	public static Voxel Magic => new(3);
+	public static Voxel Dirt => new(4);
+	public static Voxel Grass => new(5);
+	public static Voxel Object => new(6);
+
+	private Voxel(byte type)
+	{
+		m_Type = type;
+	}
+
+	public static implicit operator byte(Voxel v) => v.m_Type;
+
+	public static implicit operator Voxel(byte b) => new(b);
+}
